2c343d2b3371d53e0277b3a6ead8d0de404fe5d2
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-11-24 00:56:31 -0500

    Create R-packages.R

---
+++ R-packages.R
@@ -0,0 +1,12 @@
+#insall packages
+install.packages('package_name',dependecies=T)
+
+#Load package
+library('package_name')
+
+#R package for examplar datasets
+install.packages('datasets',dependencies=T)
+library('datasets')
+
+# The list of packages in dataset library is listed in following URL
+# https://vincentarelbundock.github.io/Rdatasets/datasets.html
