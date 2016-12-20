66cd18d3daa4b510de6e529d7fec11b34cc74db6
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 12:05:59 -0500

    Update ObjectsInR.R

--- ObjectsInR.R
+++ ObjectsInR.R
@@ -5,3 +5,28 @@
 > numericVector
 [1] 1 2 3
 
+#Creating a character vector using c() function
+> characterVector<-c("abc","def","xyz")
+> characterVector
+[1] "abc" "def" "xyz"
+
+#Creating a logical vector
+> logicalVecor<-c(TRUE,FALSE,FALSE)
+> logicalVecor
+[1]  TRUE FALSE FALSE
+
+#Creating a vector dynamically
+> characterVector<-vector(mode = "character")
+> characterVector
+character(0)
+> characterVector<-c(characterVector,"xyz")
+> characterVector
+[1] "xyz"
+> characterVector<-c(characterVector,"abc")
+> characterVector
+[1] "xyz" "abc"
+> characterVector<-c(characterVector,"def")
+> characterVector
+[1] "xyz" "abc" "def"
+
+
