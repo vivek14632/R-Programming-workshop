950ff79dfbf9b9d64c4f6d4fd794ca2a0402518e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 14:28:24 -0500

    Update dataFrame.R

--- ObjectsInR/dataFrame.R
+++ ObjectsInR/dataFrame.R
@@ -8,3 +8,19 @@
   X1 X2 X3 X4 X5
 1  1  3  5  7  9
 2  2  4  6  8 10
+
+#check class of data frame
+> class(y)
+[1] "data.frame"
+> 
+
+#check column names
+> names(y)
+[1] "X1" "X2" "X3" "X4" "X5"
+
+#update column names
+> names(y)[1]
+[1] "X1"
+> names(y)[1]<-"column1"
+> names(y)
+[1] "column1" "X2"      "X3"      "X4"      "X5" 
