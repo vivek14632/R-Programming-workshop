582a7bbf780cc4e1d618881024d478948b303451
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-21 11:40:50 -0500

    Update forLoop.R

--- loops/forLoop.R
+++ loops/forLoop.R
@@ -1,4 +1,4 @@
-
+#Example of loop 
 > for(i in 1:5)
 + {
 +   print(i)
@@ -6,5 +6,32 @@
 [1] 1
 [1] 2
 [1] 3
+[1] 4
+[1] 5
+
+
+#break in a loop
+> for(i in 1:5)
++ {
++   if(i==3)
++   {
++     break
++   }
++   print(i)
++ }
+[1] 1
+[1] 2
+
+#continue statement in R is called 'next'
+> for(i in 1:5)
++ {
++   if(i==3)
++   {
++     next
++   }
++   print(i)
++ }
+[1] 1
+[1] 2
 [1] 4
 [1] 5
