271e779c4b119d63599421bced2f415bc0ab55db
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 21:10:15 -0500

    Update standardFunctions.R

--- functions/standardFunctions.R
+++ functions/standardFunctions.R
@@ -1,1 +1,21 @@
 
+> library('MASS')
+> names(Cars93)
+ [1] "Manufacturer"       "Model"              "Type"               "Min.Price"          "Price"              "Max.Price"         
+ [7] "MPG.city"           "MPG.highway"        "AirBags"            "DriveTrain"         "Cylinders"          "EngineSize"        
+[13] "Horsepower"         "RPM"                "Rev.per.mile"       "Man.trans.avail"    "Fuel.tank.capacity" "Passengers"        
+[19] "Length"             "Wheelbase"          "Width"              "Turn.circle"        "Rear.seat.room"     "Luggage.room"      
+[25] "Weight"             "Origin"             "Make"              
+
+#Minimum Function
+> min(Cars93$Min.Price)
+[1] 6.7
+
+#Maximum function
+> max(Cars93$Min.Price)
+[1] 45.4
+
+#Standard deviation
+> sd(Cars93$Min.Price)
+[1] 8.746029
+
