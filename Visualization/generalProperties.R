846b6acadd218632d57b3f2e9cea6a87096d484e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 23:05:35 -0500

    Update generalProperties.R

--- Visualization/generalProperties.R
+++ Visualization/generalProperties.R
@@ -14,3 +14,11 @@ plot(Cars93$Min.Price,type='b',ylab = 'Price',ylim = c(0,100),main='Plot Title')
 #x-axis label - use ylab='your y-axis label'
 
 plot(Cars93$Min.Price,type='b',ylab = 'your y-axis label',xlab='your x-axis label',ylim = c(0,100),main='Plot Title')
+
+#axis() function is used to enable axis. By default, the botton and left axis are enabled.
+#To enable axis on the top and right, use the following code
+axis(side = 3)
+axis(side = 4)
+
+
+
