cdfefb92844fc3e6e1b56191a5724a32842214c2
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 21:12:18 -0500

    Create functions.R

---
+++ functions/functions.R
@@ -0,0 +1,16 @@
+#create a new function
+> mySum<-function(x,y)
++ {
++   return(x+y)
++ }
+> mySum(3,4)
+[1] 7
+
+#get body of a function - just type the name of the function
+> mySum
+function(x,y)
+{
+  return(x+y)
+}
+
+
