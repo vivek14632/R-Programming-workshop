ae2f992f8567fc8140d7fa6192ab5911e0aa5664
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 20:09:20 -0500

    Update list.R

--- ObjectsInR/list.R
+++ ObjectsInR/list.R
@@ -1,1 +1,16 @@
+#create a numeric vector
+> vector1
+ [1] 1 1 1 1 1 1 1 1 1 1
 
+#create a character vector - 'letters' and 'LETTERS' are predefined in R
+> vector2<-letters[1:5]
+> vector2
+[1] "a" "b" "c" "d" "e"
+
+> L1=list(vector1,vector2)
+> L1
+[[1]]
+ [1] 1 1 1 1 1 1 1 1 1 1
+
+[[2]]
+[1] "a" "b" "c" "d" "e"
