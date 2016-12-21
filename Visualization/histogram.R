c59657fcd4b337af9ecbbda6af045d3d4bc8c69a
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-21 12:33:05 -0500

    Update histogram.R

--- Visualization/histogram.R
+++ Visualization/histogram.R
@@ -13,3 +13,14 @@ par(mfrow=c(2,1))
 hist(Cars93$Max.Price,main='Histogram example',xlab = 'Price',breaks = 10)
 hist(Cars93$Max.Price,main='Histogram example',xlab = 'Price',breaks = 20)
 par(mfrow=c(1,1))
+
+#To plot the probability instead of frequency, use Freq=F
+hist(Cars93$Max.Price,main='Histogram example',xlab = 'Price',freq = F)
+
+#Change color of the histogram
+hist(Cars93$Max.Price,main='Histogram example',xlab = 'Price',col='blue')
+
+#change color of the border
+hist(Cars93$Max.Price,main='Histogram example',xlab = 'Price',border = 10)
+
+
