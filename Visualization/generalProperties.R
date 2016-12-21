7ba6fdc88df79b5e12cf4681aaaf715238cdd5d5
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-20 21:46:32 -0500

    Update generalProperties.R

--- Visualization/generalProperties.R
+++ Visualization/generalProperties.R
@@ -6,3 +6,11 @@ par(mai=c(10,10,1,1)/10)
 plot(Cars93$Min.Price,type='b',ylab = 'Price',ylim = c(0,100))
 par(mai=c(10,10,10,10)/10)
 plot(Cars93$Min.Price,type='b',ylab = 'Price',ylim = c(0,100))
+
+#plot title
+plot(Cars93$Min.Price,type='b',ylab = 'Price',ylim = c(0,100),main='Plot Title')
+
+#x-axis label - use xlab='your x-axis label'
+#x-axis label - use ylab='your y-axis label'
+
+plot(Cars93$Min.Price,type='b',ylab = 'your y-axis label',xlab='your x-axis label',ylim = c(0,100),main='Plot Title')
