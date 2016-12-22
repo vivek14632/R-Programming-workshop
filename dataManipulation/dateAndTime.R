04d97cc5b6ae21f05d9701f0ea3c2fb3d81dfd4e
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-21 20:04:48 -0500

    Create dateAndTime.R

---
+++ dataManipulation/dateAndTime.R
@@ -0,0 +1,14 @@
+
+#format date
+strptime('12-12-2016','%m-%d-%Y')
+[1] "2016-12-12 EST"
+
+#formatting symbols
+%m month in numeric
+%b month as names - Apr
+%d date in numeric
+%Y year in four letters
+
+> strptime('17-Dec-2016','%d-%b-%Y')
+[1] "2016-12-17 EST"
+
