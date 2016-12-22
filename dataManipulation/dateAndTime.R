8339944238fe5aa47e8a8dfd2186c10d8560c8f6
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-21 20:08:13 -0500

    Update dateAndTime.R

--- dataManipulation/dateAndTime.R
+++ dataManipulation/dateAndTime.R
@@ -12,3 +12,13 @@ strptime('12-12-2016','%m-%d-%Y')
 > strptime('17-Dec-2016','%d-%b-%Y')
 [1] "2016-12-17 EST"
 
+> weekdays(mday)
+[1] "Saturday"
+> 
+
+#currnet time
+> Sys.time()-mday
+Time difference of 4.837515 days
+> 
+
+
