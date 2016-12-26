fe172403b12f3c66263544ebc2cd7c3160672fbf
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-26 13:29:09 -0500

    Create repeatLoop.R

---
+++ loops/repeatLoop.R
@@ -0,0 +1,17 @@
+> x=0
+> repeat
++ {
++   x=as.integer(readline(prompt = 'Type any number:'))
++   
++   if(x==1)
++   {
++     break
++   }
++   print(paste('x=',x))
++ }
+Type any number:10
+[1] "x= 10"
+Type any number:3
+[1] "x= 3"
+Type any number:1
+> 
