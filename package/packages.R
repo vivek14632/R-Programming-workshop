7c7945eeef2e6b56ccbb242bc0a375ab1f15d596
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-12-26 14:43:53 -0500

    Update packages.R

--- package/packages.R
+++ package/packages.R
@@ -1,12 +1,23 @@
 #insall packages
-install.packages('package_name',dependecies=T)
-
+> install.packages("datasets", dependencies = T)
+Installing package into ‘C:/Users/ThinkPad/Documents/R/win-library/3.3’
+(as ‘lib’ is unspecified)
+Warning in install.packages :
+  package ‘datasets’ is not available (for R version 3.3.1)
+Warning in install.packages :
+  package ‘datasets’ is a base package, and should not be updat 
+    
+    
 #Load package
-library('package_name')
+> library('datasets')
 
-#R package for examplar datasets
-install.packages('datasets',dependencies=T)
-library('datasets')
+#To check detailed list of datasets: http://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html
 
-# The list of packages in dataset library is listed in following URL
-# https://vincentarelbundock.github.io/Rdatasets/datasets.html
+# Update package
+> update.packages('datasets')
+    
+> remove.packages('datasets')
+Removing package from ‘C:/Users/ThinkPad/Documents/R/win-library/3.3’
+(as ‘lib’ is unspecified)
+    
+    
