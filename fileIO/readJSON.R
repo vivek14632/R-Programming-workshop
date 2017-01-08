da9e1ca0cfbe7be24fbd50346cfc884e89f73862
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-01-07 22:22:15 -0500

    Update readJSON.R

--- fileIO/readJSON.R
+++ fileIO/readJSON.R
@@ -14,3 +14,25 @@ install.packages('RJSONIO',dependencies=T)
 library('RJSONIO')
 
 m_data<-fromJSON(content="https://raw.githubusercontent.com/vivek14632/R-Programming-workshop/master/fileIO/test.json")
+
+> m_data$BOB
+    age   state country 
+   "21"    "FL"   "USA" 
+> class(m_data$BOB)
+[1] "character"
+> m_data$BOB['age']
+ age 
+"21" 
+> as.numeric(m_data$BOB['age'])
+[1] 21
+> as.numeric(m_data$BOB['state'])
+[1] NA
+Warning message:
+NAs introduced by coercion 
+> as.character(m_data$BOB['state'])
+[1] "FL"
+> as.character(m_data$BOB['country'])
+[1] "USA"
+
+
+
