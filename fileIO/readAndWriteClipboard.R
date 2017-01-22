964f701926bbef1244ed452b45114860df65cba5
Zenfin <sm4616@gmail.com>
2017-01-22 00:40:43 -0500

    Update readAndWriteClipboard.R

--- fileIO/readAndWriteClipboard.R
+++ fileIO/readAndWriteClipboard.R
@@ -1,3 +1,7 @@
 m_data<-read.table('clipboard',header=T)
 
 write.table(m_data,file='clipboard')
+
+#read from a webpage
+http://www.iie.org/Services/Project-Atlas/United-States/International-Students-In-US#.WIRD91UrKMo
+data1<-read.table('clipboard',sep = '\t')
