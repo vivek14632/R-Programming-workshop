406521d246658c26aca17819c4547a68ed55309c
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-01-24 16:58:12 -0500

    Update facebook.R

--- socialMedia/facebook.R
+++ socialMedia/facebook.R
@@ -7,8 +7,8 @@ fb_oauth <- fbOAuth(app_id="***",
 
 fb_page <- getPage(page="subway", token=fb_oauth,n=50,feed = T,reactions = T)
 
-plot(fb_page$shares_count,type = 'b',ylab = "#Share",xlab = 'FB post‘,col=‘red’)
-plot(fb_page$likes_count[1:50],type = 'b',ylab = "#Comments",xlab = 'FB post',col=‘green')
+plot(fb_page$shares_count,type = 'b',ylab = "#Share",xlab = 'FB post',col='red')
+plot(fb_page$likes_count[1:50],type = 'b',ylab = "#Comments",xlab = 'FB post',col='green')
 plot(fb_page$comments_count[1:50],type = 'b',ylab = "#Comments",xlab = 'FB post',col='blue')
 
 barplot(c(sum(fb_page$shares_count),sum(fb_page$likes_count),sum(fb_page$comments_count)),
