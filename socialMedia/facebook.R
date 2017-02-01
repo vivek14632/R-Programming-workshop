9f3c8f26b822e721707092ebc6d7ae34557b8fcc
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-02-01 15:47:18 -0500

    Update facebook.R

--- socialMedia/facebook.R
+++ socialMedia/facebook.R
@@ -37,3 +37,19 @@ analysis=as.data.frame(cbind(likes=fb_page$likes_count,
                              angry=fb_page$angry_count
                                                  ))
 
+analysis=as.data.frame(cbind(likes=fb_page$likes_count,
+                             message=fb_page$message,
+                             create_time=fb_page$created_time,
+                             today='2017-02-01',
+                             type=fb_page$type,
+                             link=fb_page$link,
+                             comments=fb_page$comments_count,
+                             shares=fb_page$shares_count,
+                             love=fb_page$love_count,
+                             haha=fb_page$haha_count,
+                             wow=fb_page$wow_count,
+                             sad=fb_page$sad_count,
+                             angry=fb_page$angry_count
+                                                 ))
+
+
