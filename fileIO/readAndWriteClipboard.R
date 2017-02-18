963d6dc508a4f84e85fa556efc4642d02cb63490
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-02-17 19:09:41 -0500

    Update readAndWriteClipboard.R

--- fileIO/readAndWriteClipboard.R
+++ fileIO/readAndWriteClipboard.R
@@ -9,4 +9,4 @@ write.table(m_data,file='clipboard')
 
 #read from a webpage
 http://www.iie.org/Services/Project-Atlas/United-States/International-Students-In-US#.WIRD91UrKMo
-data1<-read.table('clipboard',sep = '\t')
+data1<-read.table('clipboard',sep = '\t',header=T)
