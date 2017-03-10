2ac46876f116443aebac77985ac09226963e2a7c
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-03-10 16:24:38 -0500

    Update ggplot.R

--- Visualization/ggplot.R
+++ Visualization/ggplot.R
@@ -2,3 +2,14 @@ library('ggplot2')
 
 #scatter plot
 qplot(carat,price,data=diamonds)
+
+#with log transformation
+qplot(log(carat),log(price),data = diamonds)
+
+#plot with colors
+qplot(carat,price,data = diamonds,colour=color)
+qplot(carat,price,data = diamonds,colour=cut)
+
+#plot with different shapes
+qplot(carat,price,data = diamonds,shape=cut)
+
