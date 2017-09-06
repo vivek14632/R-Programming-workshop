6e17015797d74941b60070ce2111d744c741f2a5
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-09-06 12:32:43 -0400

    Update RandomForest.R

--- DataMining/RandomForest.R
+++ DataMining/RandomForest.R
@@ -1,1 +1,22 @@
-# http://trevorstephens.com/kaggle-titanic-tutorial/r-part-5-random-forests/
+
+# Random Forest prediction of Kyphosis data
+install.packages('randomForest')
+library(randomForest)
+
+
+fit <- randomForest(Kyphosis ~ Age + Number + Start,   data=kyphosis)
+print(fit) # view results 
+importance(fit) # importance of each predictor
+
+nrow(kyphosis)
+
+training=kyphosis[1:70,]
+test=kyphosis[71:81,]
+
+fit <- randomForest(Kyphosis ~ Age + Number + Start,   data=training)
+
+# lets check the prediction
+predict(fit, test)
+
+# lets check the actual values
+test$Kyphosis
