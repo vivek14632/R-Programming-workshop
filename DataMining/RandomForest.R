97ce4b7dfb97a9e3961361d67dd70d963790ba7b
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-09-06 21:39:50 -0400

    Update RandomForest.R

--- DataMining/RandomForest.R
+++ DataMining/RandomForest.R
@@ -20,3 +20,37 @@ predict(fit, test)
 
 # lets check the actual values
 test$Kyphosis
+
+ checkChar<-function(x,y)
+ {
+   if(x==y)
+   {
+     return(TRUE)
+   }else{
+     return(FALSE)
+   }
+ }
+ 
+ prediction_RF=as.character(pred)
+ 
+ actual=as.character(test$Kyphosis)
+ 
+ actual
+ prediction_RF
+ 
+ checkChar('abc','abc')
+ 
+ checkChar('bc','abc')
+ 
+count=0
+for(i in 1:11)
+{
+  if(checkChar(prediction_RF[i],actual[i]))
+  {
+    count=count+1
+  }
+  
+}
+count
+
+print(paste('accuracy=',count/11.0))
