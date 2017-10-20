caff54b6034eb8c2e77b150104c62b85cf653008
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2017-10-20 11:02:11 -0400

    Create interactiveVisualization.R

---
+++ Visualization/interactiveVisualization.R
@@ -0,0 +1,12 @@
+https://www.statmethods.net/advgraphs/interactive.html
+
+
+Make sure you have install JDK and added to path and created JAVA_HOME system variable.
+
+Add location of jdk bin folder in your system path variable. It will look similar to the following path
+C:\Program Files\Java\jdk-9.0.1\bin
+
+Add JAVA_HOME variable in the list of system variables pointing to JDK folder. The path for JAVA_HOME will be similar to the following path
+C:\Program Files\Java\jdk-9.0.1
+
+
